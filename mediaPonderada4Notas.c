6718f4e09431601b421f821cd3668812f025cfb0
Romulo Santana <romulomsantana@hotmail.com>
2023-06-09 18:42:21 -0300

    add mediaPonderada4Notas.c

---
+++ mediaPonderada4Notas.c
@@ -0,0 +1,20 @@
+/*
+Faça um programa que leia quatro notas (valores reais) de um aluno, calcule sua 
+média ponderada utilizando, respectivamente, os pesos 1, 2, 3 e 4 para cada nota 
+e imprima uma mensagem dizendo se o aluno foi aprovado com louvor, aprovado, 
+reprovado ou deverá fazer prova final. Na sua solução utilize uma função chamada 
+AnalisarSituacao que receba 4 parâmetros (as 4 notas) e retorne a situação do 
+aluno, conforme os seguintes critérios:
+
+
+aprovado com louvor (média >= 9)
+
+aprovado (média >= 7);
+
+reprovado (média < 3);
+
+prova final ( 3 <= média < 7).
+*/
+
+#include <stdio.h>
+
