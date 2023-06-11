b2b3d7dba3b5498985cddf5c9f919ad7eb03ba79
Romulo Santana <romulomsantana@hotmail.com>
2023-06-11 15:32:13 -0300

    counts any letter now

--- contadorLetra.c
+++ contadorLetra.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
 
-int contaLetraA(const char *texto) {
+int contaLetra(const char *texto, char letra) {
   int contador = 0;
 
   for (int i = 0; texto[i] != '\0'; i++) {
     char c = tolower(texto[i]);
+    letra = tolower(letra);
 
-    if (c == 'a') {
+    if (c == letra) {
       contador++;
     }
   }
@@ -17,12 +18,16 @@ int contaLetraA(const char *texto) {
 
 int main() {
   char texto[1000];
+  char letra;
 
   printf("digite o texto: ");
   fgets(texto, sizeof(texto), stdin);
 
-  int numeroDeOcorrencias = contaLetraA(texto);
-  printf("Numero de letras a na frase: %d\n", numeroDeOcorrencias);
+  printf("digite a letra que quer contar: ");
+  scanf("%c", &letra);
+
+  int numeroDeOcorrencias = contaLetra(texto, letra);
+  printf("Numero de letras %c na frase: %d\n", letra, numeroDeOcorrencias);
 
   return 0;
 }
