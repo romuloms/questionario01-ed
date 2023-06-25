325d8cc7367b2756280eea2fbe12e150489a445e
Romulo Santana <romulomsantana@hotmail.com>
2023-06-24 23:49:59 -0300

    instance functions on header

--- fila/FilaEncadHeader.h
+++ fila/FilaEncadHeader.h
@@ -24,3 +24,27 @@ char compara(ITEM x, ITEM y);
 bool igual(ITEM x, ITEM y);
 
 void inicializar(FILA *f);
+
+int tamanho(FILA *f);
+
+bool cheia(FILA *f);
+
+bool vazia(FILA *f);
+
+bool inserir(ITEM item, FILA *f);
+
+bool frente(ITEM *item, FILA *f);
+
+bool retirar(ITEM *item, FILA *f);
+
+ITEM enesimo(int n, FILA *f);
+
+void exibirItem(ITEM i);
+
+void exibir(FILA *f);
+
+void limpar(FILA *f);
+
+void destruir(FILA *f);
+
+#endif // FILA_ENC
