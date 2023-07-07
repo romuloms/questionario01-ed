f86a418223ba3dc74473b1285565ce557e1a15e1
Romulo Santana <romulomsantana@hotmail.com>
2023-07-06 21:32:51 -0300

    populate linked list with basic functions

--- listas-encadeadas/linkedlist.py
+++ listas-encadeadas/linkedlist.py
@@ -4,7 +4,7 @@ from node import Node
 class LinkedList:
     def __init__(self):
         self.head = None
-        self.size = 0
+        self._size = 0
 
     def append(self, elem):
         if self.head:
@@ -16,4 +16,52 @@ class LinkedList:
         else:
             # primeira insercao
             self.head = Node(elem)
-        self.size = self.size + 1
+        self._size = self._size + 1
+
+    def __len__(self):
+        """Retorna o tamanho da lista"""
+        return self._size
+    
+    def get(self, index):
+        # a = lista.get(6)
+        pass
+
+    def set(self, index, elem):
+        # lista.set(5, 9)
+        pass
+
+    def __getitem__(self, index):
+        # a = lista[6]
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                raise IndexError("list index out of range")
+        if pointer:
+            return pointer.data
+        raise IndexError("list index out of range")
+
+    def __setitem__(self, index, elem):
+        # lista[5] = 9
+        pointer = self.head
+        for i in range(index):
+            if pointer:
+                pointer = pointer.next
+            else:
+                raise IndexError("list index out of range")
+        if pointer:
+            pointer.data = elem
+        else:
+            raise IndexError("list index out of range")
+        
+    def index(self, elem):
+        """Retorna o indice do elemento na lista"""
+        pointer = self.head
+        i = 0
+        while(pointer):
+            if pointer.data == elem:
+                return i
+            pointer = pointer.next
+            i = i+1
+        raise ValueError("{} is not in list".format(elem))
