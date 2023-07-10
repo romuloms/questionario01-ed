50044279a08a0ad9061f9569dae4de80e0753953
Romulo Santana <romulomsantana@hotmail.com>
2023-07-09 21:45:53 -0300

    static sequential list

---
+++ listaSequencialEstatica/listaSequencialEstatica/ListaSequencialEstatica.c
@@ -0,0 +1,162 @@
+//
+//  ListaSequencialEstatica.c
+//  listaSequencialEstatica
+//
+//  Created by Rômulo Santana on 09/07/23.
+//
+
+#include "ListaSequencialEstatica.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+
+char compare(ITEM x, ITEM y)
+{
+    return x > y ? 1 : (x < y ? -1 : 0);
+}
+
+
+bool igual(ITEM x, ITEM y)
+{
+    return compare(x,y) == 0;
+}
+
+
+void inicializar(LISTA *l)
+{
+    // a funcao recebe um ponteiro como argumento
+    l->tamanho = 0;
+    // o ponteiro acessa o endereco de memoria da variavel tamanho e altera para 0
+}
+
+
+int tamanho(LISTA *l)
+{
+    // a funcao recebe um ponteiro como argumento
+    return l->tamanho;
+    // o ponteiro acessa o endereco de memoria da variavel tamanho e
+    // a funcao retorna o valor contido nesse endereco
+}
+
+
+bool cheia(LISTA *l)
+{
+    // a funcao recebe um ponteiro como argumento
+    // depois, invoca a funcao tamanho() com o mesmo ponteiro de argumento
+    // a funcao tamanho retorna um inteiro com o valor do tamanho do vetor de itens
+    // a funcao cheia verifica se esse tamanho eh igual aa constante MAX(1000)
+    return tamanho(l) == MAX;
+}
+
+
+bool vazia(LISTA *l)
+{
+    // faz o mesmo que a funcao cheia(), mas verificando se o tamanho eh 0
+    return tamanho(l) == 0;
+}
+
+
+bool inserir(ITEM item, LISTA *l)
+{
+    if (tamanho(l) == MAX)
+        return false; // lista cheia
+    else
+       l->itens[l->tamanho++] = item;
+    // caso a lista nao esteja cheia, o ponteiro acessa o end de memoria
+    // do vetor itens, adiciona 1 unidade ao indice do vetor e aloca o item
+    // que foi passado como argumento nesse indice do vetor
+    return true;
+}
+
+
+int buscar(ITEM item, LISTA *l)
+{
+    for (int pos = 0; pos < tamanho(l); pos++)
+       if (igual(item, l->itens[pos]))
+           return pos; // achou
+    return -1; // nao achou
+}
+
+
+ITEM enesimo(int n, LISTA *l)
+{
+    if (n >= 0 && n < tamanho(l))
+       return (l->itens[n]);
+    else
+       exit(EXIT_FAILURE);
+}
+
+
+bool alterar(ITEM item, int pos, LISTA *l)
+{
+    if (pos >= 0 && pos < tamanho(l))
+    {
+        l->itens[pos] = item;
+        return true;
+    }
+    return false;
+}
+
+
+bool inserirNaPos(ITEM item, int i, LISTA *l)
+{
+    // A posicao tem que estar entre 0 e MAX-1 e ate o tamanho atual
+    if ((tamanho(l) >= MAX) || (i < 0) || (i > tamanho(l)))
+        return false; // lista cheia ou indice invalido
+
+    // Se for inserido no meio, e necessario abrir espaco para o item
+    for (int j = tamanho(l); j > i; j--)
+        l->itens[j] = l->itens[j-1];
+
+    // Coloca o item em um espaco vago e ajusta o tamanho
+    l->itens[i] = item;
+    l->tamanho++;
+    return true;
+}
+
+
+bool remover(ITEM item, LISTA *l)
+{
+    int i, j;
+    for (i = 0; i < tamanho(l); i++)
+        if (igual(item, l->itens[i]))
+        {
+            for (j = i; j < l-> tamanho - 1; j++)
+               l->itens[j] = l->itens[j + 1];
+
+            l->tamanho--;
+            return true; // achou
+        }
+    return false; // nao achou
+}
+
+
+void exibirItem(ITEM i)
+{
+    printf("%d", i);
+}
+
+
+void exibirLista(LISTA *l)
+{
+    printf("[");
+    for(int i = 0; i < tamanho(l);)
+    {
+        exibirItem(l->itens[i++]);
+        if (i < tamanho(l))
+           printf(",");
+    }
+    printf("]");
+}
+
+void limpar(LISTA *l)
+{
+    l->tamanho = 0;
+}
+
+void destruir(LISTA *l)
+{
+   limpar(l);
+}
---
+++ listaSequencialEstatica/listaSequencialEstatica/ListaSequencialEstatica.h
@@ -0,0 +1,101 @@
+//
+//  ListaSequencialEstatica.h
+//  listaSequencialEstatica
+//
+//  Created by Rômulo Santana on 09/07/23.
+//
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+#ifndef LISTA_SEQ_ESTATICA
+#define LISTA_SEQ_ESTATICA
+
+#define MAX 1000
+
+typedef int ITEM;
+
+typedef struct{
+    ITEM itens[MAX];    // vetor de inteiros com 1000 posicoes
+    int tamanho;
+} LISTA;
+
+/*
+   Compara o item x com o y, retornando:
+     1 caso x > y
+     0 caso x = y
+    -1 caso x < y
+*/
+char compare(ITEM x, ITEM y);
+
+// Retorna true se x = y e false caso contrario
+bool igual(ITEM x, ITEM y);
+
+// Inicializa a lista deixando-a pronta para ser utilizada.
+void inicializar(LISTA *l);
+
+// Retornar o tamanho da lista sequencial
+int tamanho(LISTA *l);
+
+// Retorna true se a lista esta cheia (Tamanho = MAX)
+bool cheia(LISTA *l);
+
+// Retorna true se a lista esta vazia (Tamanho = 0)
+bool vazia(LISTA *l);
+
+/*
+  Objetivo: Insere o item passado como parametro na lista passada.
+            Se o tamanho da lista ja for igual ao tamanho maximo,
+            a funcao Inserir retorna false.
+*/
+bool inserir(ITEM item, LISTA *l);
+
+/*
+  Objetivo: Busca na lista o item passado.
+            Caso encontre, retorna a posicao (entre 0 e tamanho - 1).
+            Caso nao encontre, retorna o valor -1.
+*/
+int buscar(ITEM item, LISTA *l);
+
+// Retornar o n-esimo elemento da lista sequencial
+// Pre-condicao: 0 <= n < tamanho
+ITEM enesimo(int n, LISTA *l);
+
+/*
+  Objetivo: Altera o item contido em uma dada posicao da lista.
+            Caso a posicao exista, retorna true. Caso nao exista,
+            a alteracao nao sera feita e retorna false.
+*/
+bool alterar(ITEM item, int pos, LISTA *l);
+
+/*
+  Objetivo: Inserir o item passado como parametro na posicao i da lista.
+            Caso a lista nao esteja cheia e a posicao seja valida
+            (0<=i<=tamanho), o item sera inserido e a funcao retorna true.
+            Caso contrario, a funcao retorna false para indicar que o item
+            nao foi inserido.
+ */
+bool inserirNaPos(ITEM item, int i, LISTA *l);
+
+/*
+  Objetivo: Remove o item passado da lista. Caso nao seja
+            encontrato, retorna false. Se for removido, true.
+*/
+bool remover(ITEM item, LISTA *l);
+
+// Exibicao de um item da lista
+void exibirItem(ITEM i);
+
+// Exibicao da lista sequencial
+void exibirLista(LISTA *l);
+
+// Remove todos os valores da lista, deixando-a vazia
+void limpar(LISTA *l);
+
+// Destruicao da lista sequencial
+void destruir(LISTA *l);
+
+#endif // LISTA_SEQ_ESTATICA
+
