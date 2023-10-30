774cc2e08d0b14221c17eb341aba644326aa3af9
Romulo Santana <romulomsantana@hotmail.com>
2023-10-29 23:14:36 -0300

    create nested sum algorithm

---
+++ nested_array_sum.py
@@ -0,0 +1,12 @@
+def soma_aninhado(arr):
+    # primeiro = arr[0]
+    
+    if not arr:
+        return 0
+    elif not (isinstance(arr[0], list)):
+        return arr[0] + soma_aninhado(arr[1:])
+    else:
+        return soma_aninhado(arr[0]) + soma_aninhado(arr[1:])
+
+arr = [1,2,3,4,[4,[2,2]]]
+print(soma_aninhado(arr))  
